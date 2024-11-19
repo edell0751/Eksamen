59d6d056c385c98cf1ff87f045e8c608e31d5d9a
edell0751 <edell0751@oslomet.no>
2024-11-19 11:36:38 +0100

    script

--- script.js
+++
@@ -1,9 +0,0 @@
-function myFunction(){
-    var x = document.getElementById("fullMenu");
-    if(x.style.display == block){
-        x.style.display = "none";
-    }
-    else{
-        x.style.display = "block";
-    }
-}
